fe778a8235643fb2118d0503d5c485fe6df65d43
lord-amresh <amresh2006@gmail.com>
2026-04-06 09:06:05 +0545

    4 props completed

--- src/App.jsx
+++ src/App.jsx
@@ -51,20 +51,19 @@ function AppContent () {
       >
         <h1 className='text-5xl font-bold mb-4'>React Props explained</h1>
         <p className='text-lg text-gray-300 mb-8'>A comprehensive guide to understanding props in React.</p>
-        <p>Testing the basic functionality for git.</p>
       </header>
       <div className='space-y-8'>
         <div id="basic" className='scroll-mt-200'>
           <BasicProps />
         </div>
         <div id="basic" className='scroll-mt-200'>
-          <RefProps />
-        </div>
-        <div id="basic" className='scroll-mt-200'>
           <ChildrenProps />
         </div>
         <div id="basic" className='scroll-mt-200'>
           <Complexprops />
+        </div>
+        <div id="basic" className='scroll-mt-200'>
+          <RefProps />
         </div>
         <div id="basic" className='scroll-mt-200'>
           <ThemeToggler />
